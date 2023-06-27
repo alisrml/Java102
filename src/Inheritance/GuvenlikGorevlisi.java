ea7007801f308f88418b04d81820e55f90cf0632
alisrml <117389464+alisrml@users.noreply.github.com>
2023-06-27 11:06:18 +0300

    GuvenlikGorevlisi sınıfını olusturdum.

---
+++ src/Inheritance/GuvenlikGorevlisi.java
@@ -0,0 +1,22 @@
+package Inheritance;
+
+public class GuvenlikGorevlisi extends Memur{
+    private String belge;
+
+    public GuvenlikGorevlisi(String adSoyad,String telefon,String ePosta,String departman,String mesai,String belge){
+        super(adSoyad, telefon, ePosta, departman, mesai);
+        this.belge = belge;
+    }
+
+    public void nobet(){
+        System.out.println(this.getAdSoyad()+" nobete basladi....");
+    }
+
+    public String getBelge() {
+        return belge;
+    }
+
+    public void setBelge(String belge) {
+        this.belge = belge;
+    }
+}
