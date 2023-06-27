6b26f78b2b79eae6f8d6680e0cfd9f86235faafb
alisrml <117389464+alisrml@users.noreply.github.com>
2023-06-27 10:43:02 +0300

    Asistan sınıfını olusturdum.

---
+++ src/Inheritance/Asistan.java
@@ -0,0 +1,18 @@
+package Inheritance;
+
+public class Asistan extends Akademisyen{
+    private String ofisSaati;
+
+    public Asistan(String adSoyad,String telefon,String ePosta,String bolum,String unvan,String ofisSaati){
+        super(adSoyad, telefon, ePosta, bolum, unvan);
+        this.ofisSaati = ofisSaati;
+    }
+
+    public String getOfisSaati() {
+        return ofisSaati;
+    }
+
+    public void setOfisSaati(String ofisSaati) {
+        this.ofisSaati = ofisSaati;
+    }
+}
