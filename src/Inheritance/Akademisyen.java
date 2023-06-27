752144e1a610f804e28944956f9251e5a42a4c3e
alisrml <117389464+alisrml@users.noreply.github.com>
2023-06-27 10:36:02 +0300

    Akademisyen sınıfını olusturdum.

---
+++ src/Inheritance/Akademisyen.java
@@ -0,0 +1,32 @@
+package Inheritance;
+
+public class Akademisyen extends Calisan {
+    private String bolum;
+    private String unvan;
+
+    public Akademisyen(String adSoyad,String telefon,String ePosta,String bolum,String unvan){
+        super(adSoyad,telefon,ePosta);
+        this.bolum = bolum;
+        this.unvan = unvan;
+    }
+
+    public void derseGir(){
+        System.out.println(this.getAdSoyad()+" derse girdi...");
+    }
+
+    public String getBolum() {
+        return bolum;
+    }
+
+    public void setBolum(String bolum) {
+        this.bolum = bolum;
+    }
+
+    public String getUnvan() {
+        return unvan;
+    }
+
+    public void setUnvan(String unvan) {
+        this.unvan = unvan;
+    }
+}
