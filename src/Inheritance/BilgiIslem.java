afaf1c6b8ba5590d77a62b5466f6a55c9668468e
alisrml <117389464+alisrml@users.noreply.github.com>
2023-06-27 11:02:47 +0300

    BilgiIslem sınıfını olusturdum.

---
+++ src/Inheritance/BilgiIslem.java
@@ -0,0 +1,22 @@
+package Inheritance;
+
+public class BilgiIslem extends Memur {
+    private String gorev;
+
+    public BilgiIslem(String adSoyad,String telefon,String ePosta,String departman,String mesai,String gorev) {
+        super(adSoyad, telefon, ePosta, departman, mesai);
+        this.gorev = gorev;
+    }
+
+    public void networkKurulum(){
+        System.out.println(this.getAdSoyad()+" network kurulumu yapti...");
+    }
+
+    public String getGorev() {
+        return gorev;
+    }
+
+    public void setGorev(String gorev) {
+        this.gorev = gorev;
+    }
+}
